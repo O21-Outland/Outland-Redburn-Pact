04628019ce098f71d84e127310f3f61a8ff6491a
neronix17 <39186138+neronix17@users.noreply.github.com>
2024-03-15 23:24:07 +0000

    Initial commit

---
+++ 1.4/Mods/AlienRace/Source/RedburnPact/Abilities/Ability_AscendPawn.cs
@@ -0,0 +1,31 @@
+﻿using RimWorld;
+using RimWorld.Planet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+using VFECore.Abilities;
+using Ability = VFECore.Abilities.Ability;
+
+namespace RedburnPact
+{
+    public class Ability_AscendPawn : Ability
+    {
+        public Hediff_EvolvingRace raceHediff;
+
+        public override bool ValidateTarget(LocalTargetInfo target, bool showMessages = true)
+        {
+            raceHediff = (Hediff_EvolvingRace)target.Pawn?.health?.hediffSet?.hediffs?.Find(h => h is Hediff_EvolvingRace) ?? null;
+            return base.ValidateTarget(target, showMessages) && raceHediff != null && raceHediff.CanAscend;
+        }
+
+        public override void Cast(params GlobalTargetInfo[] targets)
+        {
+            raceHediff.Ascend(raceHediff.ModExt.raceDef);
+            base.Cast(targets);
+        }
+    }
+}
---
+++ 1.4/Mods/AlienRace/Source/RedburnPact/RedburnPactMod.cs
@@ -0,0 +1,46 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace RedburnPact
+{
+    public class RedburnPactMod : Mod
+    {
+        public static RedburnPactMod mod;
+        public static RedburnPactSettings settings;
+
+        internal static string VersionDir => Path.Combine(ModLister.GetActiveModWithIdentifier("Neronix17.Outland.RedburnPact").RootDir.FullName, "Version.txt");
+        public static string CurrentVersion { get; private set; }
+
+        public RedburnPactMod(ModContentPack content) : base(content)
+        {
+            mod = this;
+            settings = GetSettings<RedburnPactSettings>();
+
+            Version version = Assembly.GetExecutingAssembly().GetName().Version;
+            CurrentVersion = $"{version.Major}.{version.Minor}.{version.Build}";
+
+            LogUtil.LogMessage($"{CurrentVersion} ::");
+
+            File.WriteAllText(VersionDir, CurrentVersion);
+
+            Harmony OuterRimHarmony = new Harmony("Neronix17.Outland.RedburnPact");
+            OuterRimHarmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        //public override string SettingsCategory() => "Outland - Redburn Pact";
+
+        //public override void DoSettingsWindowContents(Rect inRect)
+        //{
+        //    base.DoSettingsWindowContents(inRect);
+        //}
+    }
+}
